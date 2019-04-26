c2dae095f5c8fadb563ea429d09bbb7e03861251
FrankT <44277992+fet204@users.noreply.github.com>
2019-04-25 17:13:13 -0700

    four main buttons
    Made the new, save, load, and quit buttons for the text reader

--- Java Group Project/JavaGroupProject/inclass/JavaPadGUI.java
+++ Java Group Project/JavaGroupProject/inclass/JavaPadGUI.java
@@ -1,5 +1,10 @@
 package inclass;
 
 public class JavaPadGUI {
+	public static void main(String[] args) {
+		
+	}
+	
+	
 
 }
--- Java Group Project/JavaGroupProject/inclass/JavaPadMain.java
+++ Java Group Project/JavaGroupProject/inclass/JavaPadMain.java
@@ -1,5 +1,8 @@
 package inclass;
 
+//This program provides a simple notepad-like editor.
 public class JavaPadMain {
-
+	public static void main(String[] args) {
+		JavaPadGUI gui = new JavaPadGUI();
+	}
 }

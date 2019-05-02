79567c8c17c03446397de1f7a601bba78a8af6d3
FrankT <44277992+fet204@users.noreply.github.com>
2019-05-02 16:05:26 -0700

    quit button
    title

--- Java Group Project/JavaGroupProject/inclass/JavaPadGUI.java
+++ Java Group Project/JavaGroupProject/inclass/JavaPadGUI.java
@@ -1,5 +1,8 @@
 package inclass;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 
@@ -14,28 +17,51 @@ public class JavaPadGUI {
 			window.setSize(new Dimension(500,500));
 			window.setVisible(true);
 			
-			//window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			/*test*/
+			
+			// NEW BUTTON
 			JButton buttonNew = new JButton();
 			buttonNew.setText("New");
 			buttonNew.setSize(new Dimension(200, 200));
 			northPanel.add(buttonNew);
 			
+			
+			// SAVE BUTTON 
 			JButton buttonSave = new JButton();
 			buttonSave.setText("Save");
 			northPanel.add(buttonSave);
 			
+			
+			// LOAD BUTTON
 			JButton buttonLoad = new JButton();
 			buttonLoad.setText("Load");
 			northPanel.add(buttonLoad);
 			
+			
+			// QUIT BUTTON 
 			JButton buttonQuit = new JButton();
 			buttonQuit.setText("Quit");
-			//buttonQuit.addActionListener( );
 			northPanel.add(buttonQuit);
+			
+			// Vince action
+			buttonQuit.addActionListener(new ActionListener(){
+				 @Override
+				    public void actionPerformed(ActionEvent e) {
+				        int choice = JOptionPane.showConfirmDialog(null, "Quitting; Save?");
+				        
+				        /*
+				        if (JOptionPane.YES_OPTION) {
+				        	JOptionPane.showMessageDialog(null,  "message", title, messageType);
+				        }
+				        */
+				    }
+			});
+			
+			
 			
 			window.add(northPanel, BorderLayout.NORTH);
 			
+			
+			// Fatimas bottom text.
 			southPanel.add(new JLabel("Microsoft: Resistance is frutile"));
 	        window.add(southPanel,BorderLayout.SOUTH);
 	        southPanel.setVisible(true);

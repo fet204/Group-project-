da9ebee7467850b42d5a795ea90b232eb506dc9a
FrankT <44277992+fet204@users.noreply.github.com>
2019-05-02 15:52:03 -0700

    reviving fatima class in branch
    title

---
+++ Java Group Project/JavaGroupProject/inclass/fatimaTest.java
@@ -0,0 +1,35 @@
+package inclass;
+import java.awt.*;
+import javax.swing.*;
+
+public class fatimaTest {
+	public static void main(String[] args) {
+		 JFrame window=new JFrame();
+		 JPanel southPanel=new JPanel(new FlowLayout());
+		 JScrollPane areaScrollPane = new JScrollPane(textArea);
+
+        window.setForeground(Color.blue);
+        window.setLocation(new Point(0,0));
+        window.setSize(new Dimension(700,700));
+        window.setTitle("Microsoft JavaPad XP");
+        window.setVisible(true);
+        window.setLayout(new BorderLayout());
+        JPanel centerPanel = new JPanel(new GridLayout(25,10));
+       
+       
+        window.add(centerPanel, BorderLayout.CENTER);
+      
+        
+        
+        southPanel.add(new JLabel("Microsoft: Resistance is frutile"));
+        window.add(southPanel,BorderLayout.SOUTH);
+        southPanel.setVisible(true);
+        
+        areaScrollPane.setVerticalScrollBarPolicy(
+                        JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        areaScrollPane.setPreferredSize(new Dimension(250, 250));
+		 
+		
+		
+	}
+}

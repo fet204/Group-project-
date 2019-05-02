be60c2e90e614a42ac2b3636676455fe93adb43a
DaffodilPlate67 <vincerosane@outlook.com>
2019-05-01 23:15:30 -0700

    My branch
    Instead of launching the app from the GUI file, we launch through the main and just use the constructor. No need for main and constructor is faster anyways unless we wanna do procedural stuff.
    
    Added functionality of closing the application. Pull and merge this branch anytime

--- Java Group Project/JavaGroupProject/inclass/JavaPadGUI.java
+++ Java Group Project/JavaGroupProject/inclass/JavaPadGUI.java
@@ -1,10 +1,57 @@
 package inclass;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
 
 public class JavaPadGUI {
-	public static void main(String[] args) {
-		
+	
+	public JavaPadGUI() {
+		showGUI();
 	}
 	
+	public void showGUI() {
+		JFrame window = new JFrame();
+		
+		window.setLocation(new Point(0,0));
+		window.setSize(new Dimension(500,500));
+		window.setVisible(true);
+		
+		//window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton buttonNew = new JButton();
+		buttonNew.setText("New");
+		buttonNew.setSize(new Dimension(200, 200));
+		window.add(buttonNew);
+		
+		JButton buttonSave = new JButton();
+		buttonSave.setText("Save");
+		window.add(buttonSave);
+		
+		JButton buttonLoad = new JButton();
+		buttonLoad.setText("Load");
+		window.add(buttonLoad);
+		
+		JButton buttonQuit = new JButton();
+		buttonQuit.setText("Quit");
+		//buttonQuit.addActionListener( );
+		window.add(buttonQuit);
+		buttonQuit.addActionListener(new ActionListener(){
+			 @Override
+			    public void actionPerformed(ActionEvent e) {
+			        System.exit(1);
+			    }
+		});
+		
+		//ActionListener listener = new MyActionLIstener();
+		
+		window.setLayout(new FlowLayout());
+	}
+	
+	//public void actionPerformed(ActionEvent event) {
+		
+	//}
 	
 
 }

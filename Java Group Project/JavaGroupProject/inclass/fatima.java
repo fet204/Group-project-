68b76a7f403dcf2652ae34dacb2de48bd2ed9cb0
FrankT <44277992+fet204@users.noreply.github.com>
2019-05-02 15:21:52 -0700

    adding fatimas code
    title

--- Java Group Project/JavaGroupProject/inclass/fatima.java
+++ Java Group Project/JavaGroupProject/inclass/fatima.java
@@ -1,5 +1,30 @@
 package inclass;
+import java.awt.*;
+import javax.swing.*;
 
 public class fatima {
+	public static void main(String[] args) {
+		 JFrame window=new JFrame();
+		 JPanel southPanel=new JPanel(new FlowLayout());
 
+        window.setForeground(Color.blue);
+        window.setLocation(new Point(0,0));
+        window.setSize(new Dimension(700,700));
+        window.setTitle("Microsoft JavaPad XP");
+        window.setVisible(true);
+        window.setLayout(new BorderLayout());
+        JPanel centerPanel = new JPanel(new GridLayout(25,10));
+       
+       
+        window.add(centerPanel, BorderLayout.CENTER);
+      
+        
+        
+        southPanel.add(new JLabel("Microsoft: Resistance is frutile"));
+        window.add(southPanel,BorderLayout.SOUTH);
+        southPanel.setVisible(true);
+		 
+		
+		
+	}
 }

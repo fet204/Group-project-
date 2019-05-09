8265dd394592504762abfecf40c0279720b21ea1
DaffodilPlate67 <vincerosane@outlook.com>
2019-05-08 23:39:53 -0700

    Remove dupli

--- Java Group Project/JavaGroupProject/org/project/JavaPadMain.java
+++
@@ -1,8 +0,0 @@
-package org.project;
-
-//This program provides a simple notepad-like editor.
-public class JavaPadMain {
-	public static void main(String[] args) {
-		JavaPadGUI gui = new JavaPadGUI();
-	}
-}

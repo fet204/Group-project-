fc55c39519c76eedb154d0efce30722e77d9819a
FrankT <44277992+fet204@users.noreply.github.com>
2019-05-30 15:37:29 -0700

    Commenting fatima code
    title

--- Java Group Project/JavaGroupProject/inclass/fatima.java
+++ Java Group Project/JavaGroupProject/inclass/fatima.java
@@ -6,7 +6,7 @@ public class fatima {
 	public static void main(String[] args) {
 		 JFrame window=new JFrame();
 		 JPanel southPanel=new JPanel(new FlowLayout());
-		 JScrollPane areaScrollPane = new JScrollPane(textArea);
+		 //JScrollPane areaScrollPane = new JScrollPane(textArea);
 
         window.setForeground(Color.blue);
         window.setLocation(new Point(0,0));
@@ -25,9 +25,9 @@ public class fatima {
         window.add(southPanel,BorderLayout.SOUTH);
         southPanel.setVisible(true);
         
-        areaScrollPane.setVerticalScrollBarPolicy(
-                        JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        areaScrollPane.setPreferredSize(new Dimension(250, 250));
+//        areaScrollPane.setVerticalScrollBarPolicy(
+//                        JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+//        areaScrollPane.setPreferredSize(new Dimension(250, 250));
 		 
 		
 		
